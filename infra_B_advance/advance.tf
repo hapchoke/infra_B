de9e464f440a6a2b16783bb79d90c5d2c4f93990
hapchoke <80566116+hapchoke@users.noreply.github.com>
2022-10-31 22:30:01 +0900

    create advance.tf

---
+++ infra_B_advance/advance.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "own-my-terraform-bucket-1.0"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
